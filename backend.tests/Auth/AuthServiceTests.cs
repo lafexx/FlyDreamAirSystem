364c33e8f67f286d144d5c8932a6a11f8a5bcf44
Lewis <88146791+lafexx@users.noreply.github.com>
2025-05-06 19:55:40 +1000

    wrote backend unit tests

---
+++ backend.tests/Auth/AuthServiceTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+using backend.Services.Auth;
+
+using Moq;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.tests.Auth
+{
+    public class AuthServiceFixture
+    {
+        public IWebHostEnvironment environment;
+        public AuthService authService;
+
+        public AuthServiceFixture()
+        {
+            var environmentMock = new Mock<IWebHostEnvironment>();
+
+            environmentMock
+                .Setup(e => e.WebRootPath)
+                .Returns("TestData");
+
+            environment = environmentMock.Object;
+
+            authService = new AuthService(environment);
+        }
+    }
+
+    public class AuthServiceTests: IClassFixture<AuthServiceFixture>
+    {
+        private readonly AuthServiceFixture _fixture;
+        private readonly AuthService _auth;
+
+        public AuthServiceTests(AuthServiceFixture fixture)
+        {
+            _fixture = fixture;
+            _auth = fixture.authService;
+
+            string usersFilePath = Path.Combine(_fixture.environment.WebRootPath, "users.json");
+            File.WriteAllText(usersFilePath, "[{\"username\": \"test_username\", \"email\": \"test_username@test.com\", \"password\": \"testpassword123\"}]");
+        }
+
+        [Fact(DisplayName = "rejects a sign up request if the requested username is already taken")]
+        public async Task Signup_RejectsIfUsernameIsTaken()
+        {
+            SignupRequest request = new SignupRequest
+            {
+                username = "test_username",
+                email = "test_username@test.com",
+                password = "testpassword123"
+            };
+
+            IActionResult result = await _auth.Signup(request);
+
+            BadRequestObjectResult badReq = Assert.IsType<BadRequestObjectResult>(result);
+            dynamic value = badReq.Value;
+            Assert.Equal("A user with the same username already exists!", value.message);
+        }
+
+        [Fact(DisplayName = "rejects a sign up request if the requested email is already taken")]
+        public async Task Signup_RejectsIfEmailIsTaken()
+        {
+            SignupRequest request = new SignupRequest
+            {
+                username = "unique_username",
+                email = "test_username@test.com",
+                password = "testpassword123"
+            };
+
+            IActionResult result = await _auth.Signup(request);
+
+            BadRequestObjectResult badReq = Assert.IsType<BadRequestObjectResult>(result);
+            dynamic value = badReq.Value;
+            Assert.Equal("A user with the same email already exists!", value.message);
+        }
+
+        [Fact(DisplayName = "successfully signs a user up if the request is valid")]
+        public async Task Signup_AcceptsValidRequests()
+        {
+            SignupRequest request = new SignupRequest
+            {
+                username = "unique_username",
+                email = "unique_username@test.com",
+                password = "uniquepassword123"
+            };
+
+            IActionResult result = await _auth.Signup(request);
+
+            OkObjectResult okRes = Assert.IsType<OkObjectResult>(result);
+            dynamic value = okRes.Value;
+            Assert.Equal("Sucessfully registered user.", value.message);
+        }
+    }
+}
---
+++ backend.tests/Flight/FlightServiceTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+using backend.Models;
+using backend.Services.Flight;
+using backend.Types;
+
+using Moq;
+
+namespace backend.tests.Flight
+{
+    public class FlightServiceFixture
+    {
+        public FlightService flightService;
+
+        public FlightServiceFixture()
+        {
+            var environmentMock = new Mock<IWebHostEnvironment>();
+            environmentMock
+                .Setup(e => e.WebRootPath)
+                .Returns("TestData");
+
+            flightService = new FlightService(environmentMock.Object);
+        }
+    }
+    
+    public class FlightServiceTests: IClassFixture<FlightServiceFixture>
+    {
+        private readonly FlightService _flightService;
+
+        public FlightServiceTests(FlightServiceFixture fixture)
+        {
+            _flightService = fixture.flightService;
+        }
+
+        [Fact]
+        public async Task BookFlight_SuccessfullyBooksFlight()
+        {
+            // Arrange
+            var request = new BookFlightRequest
+            {
+                username = "test_username",
+                departureLocation = new Location { country = "France", city = "Paris", airport = "Charles de Gaulle" },
+                destination = new Location { country = "USA", city = "Boston", airport = "Logan International Airport" },
+                departureDate = "01/05/2025",
+                price = 1500.0,
+                airline = "Air France",
+                seats = [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                ]
+            };
+
+            // Act
+            var result = await _flightService.BookFlight(request) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Contains("Successfully booked", result.Value.ToString());
+        }
+
+        [Fact]
+        public async Task GetBookedFlightsByUsername_ReturnsBookedFlights()
+        {
+            // Arrange
+            string username = "test_username";
+
+            // Act
+            var result = await _flightService.GetBookedFlightsByUsername(username) as OkObjectResult;
+            var flights = Assert.IsAssignableFrom<List<backend.Models.Flight>>(result.Value);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.All(flights, f => Assert.Equal(username, f.username));
+        }
+
+        [Fact]
+        public async Task CancelFlight_RemovesFlightFromFlightData()
+        {
+            // Arrange
+            string username = "test_username";
+            string flightIdToCancel = "4766453f-6af5-4eb2-ae1f-4bd18b7e534d";
+
+            // Act
+            var result = await _flightService.CancelFlight(username, flightIdToCancel) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Contains("successfully cancelled", result.Value.ToString());
+
+            var checkResult = await _flightService.GetFlightByUsernameAndId(username, flightIdToCancel) as OkObjectResult;
+            Assert.Null(checkResult.Value); 
+        }
+    }
+}
